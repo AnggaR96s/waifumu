569f91f097b89967466ffe6a4e82ac55929b8b2f
S3T17 <32949125+S3T17@users.noreply.github.com>
2019-03-02 14:31:03 +0700

    Update precache-manifest.c18e0c9992106f7c717e53d18c12d00b.js

--- precache-manifest.c18e0c9992106f7c717e53d18c12d00b.js
+++ precache-manifest.c18e0c9992106f7c717e53d18c12d00b.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
     "revision": "d7cd80495748112e5afb86600eb0c802",
-    "url": "/ashiyaap/static/media/yes.d7cd8049.svg"
+    "url": "/waifumu/static/media/yes.d7cd8049.svg"
   },
   {
     "revision": "b3b650b76a3650f921495a89887d79ef",
-    "url": "/ashiyaap/static/media/ashiap.b3b650b7.mp3"
+    "url": "/waifumu/static/media/ashiap.b3b650b7.mp3"
   },
   {
     "revision": "94269f947340696acca6",
-    "url": "/ashiyaap/static/js/runtime~main.94269f94.js"
+    "url": "/waifumu/static/js/runtime~main.94269f94.js"
   },
   {
     "revision": "2ff127ee27edf4adc339",
-    "url": "/ashiyaap/static/js/main.2ff127ee.chunk.js"
+    "url": "/waifumu/static/js/main.2ff127ee.chunk.js"
   },
   {
     "revision": "e2a032349bc2797c4db5",
-    "url": "/ashiyaap/static/js/2.e2a03234.chunk.js"
+    "url": "/waifumu/static/js/2.e2a03234.chunk.js"
   },
   {
     "revision": "2ff127ee27edf4adc339",
-    "url": "/ashiyaap/static/css/main.6fc31558.chunk.css"
+    "url": "/waifumu/static/css/main.6fc31558.chunk.css"
   },
   {
     "revision": "d1961e3969a99638e0e1dc70a6c73584",
-    "url": "/ashiyaap/index.html"
+    "url": "/waifumu/index.html"
   }
 ];
