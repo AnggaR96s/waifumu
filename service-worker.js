de2280f26b43771b853f32da00a6307bd2f3d254
S3T17 <32949125+S3T17@users.noreply.github.com>
2019-03-02 14:33:39 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ashiyaap/precache-manifest.c18e0c9992106f7c717e53d18c12d00b.js"
+  "/waifumu/precache-manifest.c18e0c9992106f7c717e53d18c12d00b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ashiyaap/index.html", {
+workbox.routing.registerNavigationRoute("/waifumu/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
